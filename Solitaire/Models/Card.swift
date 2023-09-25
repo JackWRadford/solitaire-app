539654135f8cbca77795386d7b96ad0af1121106
Jack Radford <jack.radford@live.co.uk>
2023-09-25 23:32:09 +0100

    Made Card Equatable Added Int rawValues to Rank Added colour to Suit Added SolitaireGame Pile enum Added SolitaireGame move card functions Added SolitaireGame is complete function

--- Solitaire/Models/Card.swift
+++ Solitaire/Models/Card.swift
@@ -8,19 +8,43 @@
 import Foundation
 
 extension SolitaireGame {
-    struct Card {
+    struct Card: Equatable {
         let id: UUID
         let rank: Rank
         let suit: Suit
         var isFaceUp = false
         
+        static func == (lhs: Card, rhs: Card) -> Bool {
+            return lhs.id == rhs.id
+        }
+        
         enum Suit: CaseIterable {
             case spade, club, diamond, heart
+            
+            var color: String {
+                switch self {
+                case .club, .spade:
+                    return "black"
+                case .diamond, .heart:
+                    return "red"
+                }
+            }
         }
-        enum Rank: CaseIterable {
-            case ace, two, three, four,
-                 five, six, seven , eight,
-                 nine, ten, jack, queen, king
+        
+        enum Rank: Int, CaseIterable {
+            case ace = 1
+            case two = 2
+            case three = 3
+            case four = 4
+            case five = 5
+            case six = 6
+            case seven = 7
+            case eight = 8
+            case nine = 9
+            case ten = 10
+            case jack = 11
+            case queen = 12
+            case king = 13
         }
     }
 }
--- Solitaire/Models/SolitaireGame.swift
+++ Solitaire/Models/SolitaireGame.swift
@@ -49,4 +49,90 @@ struct SolitaireGame {
             }
         }
     }
+    
+    enum Pile {
+        case tableau(column: Int)
+        case foundation(suit: Card.Suit)
+        case stock
+        case waste
+    }
+    
+    mutating func moveSelected(cards: [Card], from source: Pile, to destination: Pile) {
+        guard let bottomCard = cards.first else { return }
+        let onlyOneCard = cards.count == 1
+        switch (source, destination) {
+        case (.waste, .tableau(let destinationColumn)):
+            if canMoveCardToTableau(bottomCard, column: destinationColumn) {
+                removeCardFromWaste(bottomCard)
+                addCardsToTableau([bottomCard], at: destinationColumn)
+            }
+        case (.waste, .foundation(let destinationSuit)):
+            if canMoveCardToFoundation(bottomCard, for: destinationSuit) {
+                removeCardFromWaste(bottomCard)
+                addCardToFoundation(bottomCard, at: destinationSuit)
+            }
+        case (.tableau(let sourceColumn), .tableau(let destinationColumn)):
+            if canMoveCardToTableau(bottomCard, column: destinationColumn) {
+                removeCardsFromTableau(cards, at: sourceColumn)
+                addCardsToTableau(cards, at: destinationColumn)
+            }
+        case (.tableau(let sourceColumn), .foundation(let destinationSuit)):
+            guard onlyOneCard else { return }
+            if canMoveCardToFoundation(bottomCard, for: destinationSuit) {
+                removeCardsFromTableau([bottomCard], at: sourceColumn)
+                addCardToFoundation(bottomCard, at: destinationSuit)
+            }
+        case (.foundation(let sourceSuit), .tableau(let destinationColumn)):
+            guard onlyOneCard else { return }
+            if canMoveCardToTableau(bottomCard, column: destinationColumn) {
+                foundations[sourceSuit]?.removeAll { $0.id == bottomCard.id }
+                addCardsToTableau([bottomCard], at: destinationColumn)
+            }
+        default:
+            break
+        }
+    }
+    
+    private mutating func removeCardFromWaste(_ card: Card) {
+        waste.removeAll { $0.id == card.id }
+    }
+    
+    private mutating func removeCardsFromTableau(_ cards: [Card], at column: Int) {
+        let sourceColumnCards = tableau[column]
+        tableau[column] = sourceColumnCards.filter { !cards.contains($0) }
+    }
+    
+    private mutating func addCardsToTableau(_ cards: [Card], at column: Int) {
+        tableau[column].append(contentsOf: cards)
+    }
+    
+    private mutating func addCardToFoundation(_ card: Card, at suit: Suit) {
+        foundations[suit]?.append(card)
+    }
+    
+    private func canMoveCardToTableau(_ card: Card, column: Int) -> Bool {
+        guard column < tableau.count else { return false }
+        if let destinationCard = tableau[column].last {
+            return card.suit.color != destinationCard.suit.color && card.rank.rawValue == destinationCard.rank.rawValue - 1
+        } else {
+            return card.rank == .king
+        }
+    }
+    
+    private func canMoveCardToFoundation(_ card: Card, for suit: Suit) -> Bool {
+        guard let foundation = foundations[suit] else { return false }
+        if let destinationCard = foundation.last {
+            return card.suit == destinationCard.suit && card.rank.rawValue == destinationCard.rank.rawValue + 1
+        } else {
+            return card.rank == .ace
+        }
+    }
+    
+    func isComplete() -> Bool {
+        for suit in Suit.allCases {
+            guard let foundation = foundations[suit] else { return false }
+            return foundation.count == 13
+        }
+        return false
+    }
 }
